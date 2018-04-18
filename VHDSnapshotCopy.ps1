1b8faba613e7939d05d972c0e61517109a7a85b1
gutzeit1106 <gutzeit.11.06@gmail.com>
2018-04-18 16:07:27 +0900

    rename

---
+++ VHDSnapshotCopy.ps1
@@ -0,0 +1,17 @@
+﻿# パラメータ
+$SourceStorageAccountName = "xxxxxx" #コピー元ストレージアカウント名
+$SourceContainerName = "vhds" #コピー元コンテナー名
+$SourceBlobName = "xxxxxx.vhd" #コピー元 VHD ファイル名
+$SourceStorageAccountKey = "xxxxxx" #コピー元ストレージアカウントキー
+
+$DestStorageAccountName = "xxxxxx" #コピー先ストレージアカウント名
+$DestContainerName = "vhds" #コピー先コンテナー名
+$DestBlobName = "xxxxxx.vhd" #コピー先 VHD ファイル名
+$DestStorageAccountKey = "xxxxxx" #コピー先ストレージアカウントキー
+
+# VHD ファイルのスナップショット取得(コピー)実施
+$Ctx = New-AzureStorageContext -StorageAccountName $SourceStorageAccountName -StorageAccountKey $SourceStorageAccountKey
+$blob = Get-AzureStorageBlob -Context $Ctx -Container $SourceContainerName -Blob $SourceBlobName
+$snap = $blob.ICloudBlob.CreateSnapshot()
+$DestContext = New-AzureStorageContext -StorageAccountName $DestStorageAccountName -StorageAccountKey $DestStorageAccountKey
+Start-AzureStorageBlobCopy -SrcUri $snap.SnapshotQualifiedUri -SrcContext $Ctx -DestContext $DestContext -DestContainer $DestContainerName -DestBlob $DestBlobName
