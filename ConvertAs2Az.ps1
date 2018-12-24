1160dd6506c63255495a5f872f027f58b16e995f
gutzeit1106 <gutzeit.11.06@gmail.com>
2018-12-24 12:36:33 +0900

    Create New Script(Availability Set To Az)

---
+++ ConvertAs2Az.ps1
@@ -0,0 +1,6 @@
+﻿#ログイン
+Login-AzureRmAccount
+
+#サブスクリプションを選択
+$srcSub = Get-AzureRmSubscription | Out-GridView -Title "Select Your Subscription ..." -PassThru
+Select-AzureRmSubscription  $srcSub.SubscriptionId
