727c641e0017436beaeb8965b3610f15004c7c05
gutzeit1106 <gutzeit.11.06@gmail.com>
2019-01-11 21:15:52 +0900

    new update vmss to attach Instance Level Public IP

---
+++ UpateVMSSwithILPIP.ps1
@@ -0,0 +1,20 @@
+﻿#Parameter
+$resourceGroupName = "<ResourceGroupName>"
+$vmssName = "<ScaleSetName>"
+
+#Update VMSS Network Config
+$targetVmss = Get-AzureRmVmss -ResourceGroupName $resourceGroupName -VMScaleSetName $vmssName
+$oldConfig = $targetVmss.VirtualMachineProfile.NetworkProfile.NetworkInterfaceConfigurations[0].IpConfigurations[0]
+
+$newvmssipconfig = New-AzureRmVmssIpConfig -Name $oldConfig.Name`
+  -LoadBalancerBackendAddressPoolsId  $oldConfig.LoadBalancerBackendAddressPools[0].Id `
+  -LoadBalancerInboundNatPoolsId $oldConfig.LoadBalancerInboundNatPools[0].Id `
+  -SubnetId $oldConfig.Subnet.Id `
+  -PublicIPAddressConfigurationName "instancepublicip" `
+  -PublicIPAddressConfigurationIdleTimeoutInMinutes 10 
+
+$targetVmss.VirtualMachineProfile.NetworkProfile.NetworkInterfaceConfigurations[0].IpConfigurations[0] = $newvmssipconfig
+Update-AzureRmVmss -ResourceGroupName $resourceGroupName -VMScaleSetName $vmssName -VirtualMachineScaleSet $targetVmss
+
+#Manual Upgrade for each instances
+Get-AzureRmVmssVM -ResourceGroupName $resourceGroupName -Name $vmssName | Update-AzureRmVmssInstance -ResourceGroupName $resourceGroupName -VMScaleSetName $vmssName 
