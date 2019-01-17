e51cce7ce81d4452d4d73810fc0bc5569df75d7a
gutzeit1106 <gutzeit.11.06@gmail.com>
2019-01-17 19:19:10 +0900

    list all snapshots in azure storage account

--- ListBlobSnapshot.ps1
+++ ListBlobSnapshot.ps1
@@ -1,12 +1,9 @@
-﻿# パラメータ
-$StorageAccountName = "xxxx" #コピー元ストレージアカウント名
-$ContainerName = "xxxx" #コピー元コンテナー名
-$BlobName = "xxxx" #コピー元 VHD ファイル名
-$StorageAccountKey = "xxxx" #コピー元ストレージアカウントキー
+﻿$StorageAccountName = "xxxx" #ストレージアカウント名
+$StorageAccountKey = "xxxxx" #ストレージアカウントキー
  
 
-# ファイルのスナップショット一覧
+#スナップショット一覧
 $Ctx = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
-$blob = Get-AzureStorageBlob -Context $Ctx -Container $ContainerName -Blob $BlobName
-$ListBlob = Get-AzureStorageBlob –Context $Ctx -Prefix $BlobName -Container $ContainerName | Where-Object {$_.ICloudBlob.IsSnapshot -and $_.Name -eq $BlobName -and $_.SnapshotTime -ne $null }
-$ListBlob
+$blobs = Get-AzureStorageContainer -Context $Ctx |  Get-AzureStorageBlob -Context $Ctx
+$ListSnapshot = $blobs | Where-Object {$_.ICloudBlob.IsSnapshot -and $_.SnapshotTime -ne $null }
+$ListSnapshot | select Name, SnapshotTime
