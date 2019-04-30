8ae2335ad8b3137b1d238e806993d9f658d0b8f4
gutzeit1106 <gutzeit.11.06@gmail.com>
2019-04-30 16:26:27 +0900

    To delete all empty resource group in the specified subscription

---
+++ RemoveAllemptyResourceGroups.ps1
@@ -0,0 +1,10 @@
+﻿$resourceGroups = Get-AzureRmResourceGroup
+foreach ($rg in $resourceGroups)
+{
+    $resources = Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName
+
+    if($resources.count -le 0)
+    {
+        Remove-AzureRmResourceGroup -Name $rg.ResourceGroupName -AsJob
+    }
+}
