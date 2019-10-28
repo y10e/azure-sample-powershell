96aac62e893adee62d1733f65dc50d6364d658e0
gutzeit1106 <gutzeit.11.06@gmail.com>
2019-10-28 10:07:56 +0900

    add Swaping disk script

---
+++ SwapUnmanagedOSDiskps1.ps1
@@ -0,0 +1,12 @@
+﻿$ResourceGroupName = "xxxx" #リソースグループ名
+$VmName = "xxxx" #仮想マシン名
+$BackupVhdUri = "https://xxxx.blob.core.windows.net/xxx/xxxx.vhd" #VHDファイルのパス
+
+# Login
+Connect-AzAccount
+$mySub = Get-AzSubscription | Out-GridView -Title "Select an Azure Subscription ..." -PassThru
+Select-AzSubscription -SubscriptionId $mySub.Id
+
+$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VmName
+$Vm.StorageProfile.OsDisk.Vhd.Uri = $BackupVhdUri　#交換用ディスクのパス
+Update-AzVM -ResourceGroupName $ResourceGroupName -VM $Vm
